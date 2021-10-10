7d9f151a6a7a3eef484f03722bfdbe73f14f1779
Samyak Jain <samyakjainking@gmail.com>
2021-10-10 21:32:18 +0530

    Adding DB USERNAME AND PWD V3

--- database/initializeDatabase.js
+++ database/initializeDatabase.js
@@ -1,14 +1,14 @@
 const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize(
-    process.env.DATABSE_NAME,
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'postgres'
-    }
-);
+const sequelize = new Sequelize('postgres://dmcqlgjuvrwjeu:900d27b419ed79193ecec5fb9bfb0ac9604611af35a15f7541beea54a8c96093@ec2-35-171-171-27.compute-1.amazonaws.com:5432/d8dt0mfi8mt22') 
+// const sequelize = new Sequelize(
+//     process.env.DATABSE_NAME,
+//     process.env.DATABASE_USERNAME,
+//     process.env.DATABASE_PASSWORD,
+//     {
+//         host: 'localhost',
+//         dialect: 'postgres'
+//     }
+// );
 
 sequelize.sync();
 
