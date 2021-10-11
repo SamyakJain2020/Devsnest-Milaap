fbedf09b23cdb43c4e00c35c137c289f99ee3779
Samyak Jain <samyakjainking@gmail.com>
2021-10-11 10:08:02 +0530

    Testing DB Connection

--- database/initializeDatabase.js
+++ database/initializeDatabase.js
@@ -1,25 +1,36 @@
-const {Sequelize} = require('sequelize');
+// const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DATABSE_NAME,
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'postgres'
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.DATABSE_NAME,
+//     process.env.DATABASE_USERNAME,
+//     process.env.DATABASE_PASSWORD,
+//     {
+//         host: 'localhost',
+//         dialect: 'postgres'
+//     }
+// );
 
-sequelize.sync();
+// sequelize.sync();
 
-//connecting to POSTGRES
-(async function() {
-    try {
-        await sequelize.authenticate();
-        console.log(`DB ${process.env.DATABSE_NAME}, CONNECTED SUCCESSFULLY`);
-    } catch(err) {
-        console.log(`Unablt to connect Database`);
-    }
-})();
+// //connecting to POSTGRES
+// (async function() {
+//     try {
+//         await sequelize.authenticate();
+//         console.log(`DB ${process.env.DATABSE_NAME}, CONNECTED SUCCESSFULLY`);
+//     } catch(err) {
+//         console.log(`Unablt to connect Database`);
+//     }
+// })();
 
-module.exports = sequelize;
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+module.exports = Client;
